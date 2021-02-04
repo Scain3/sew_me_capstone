7a6b45089d86c623a1cd03d1db237fcdb7a5b0cc
Safiya Cain <say.cain3@gmail.com>
2021-02-04 13:00:01 -0500

    create backend routes to get all patterns and to get a single pattern

--- backend/routes/api/patterns.js
+++ backend/routes/api/patterns.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
-const { Patterns } = require("../../db/models");
+const { Pattern } = require("../../db/models");
 
+
+//Get all of the patterns from the pattern table
 router.get('/', asyncHandler(async(req, res) => {
-    const allPatterns = await Patterns.findAll();
+    const allPatterns = await Pattern.findAll();
     res.json({
-        product: allPatterns
+        allPatterns
     })
 }));
+
+//Get a single pattern that matches the id
+router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const eachPattern = await Pattern.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.json({
+        eachPattern
+    })
+}))
+
+module.exports = router;
