7457137bfb132522fc947f2121df64b58c9725e7
Safiya Cain <say.cain3@gmail.com>
2021-02-05 22:36:14 -0500

    style home page

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -9,6 +9,9 @@ const HomePage = () => {
                 <div className="top-main__header">Discover a New Shopping Experience</div>
                 <div className="signup-container"><NavLink className="signup" to="/signup">Sign Up</NavLink></div>
             </div>
+            <div className="paragraph-container">
+                <p className="home-paragrah__text">This is where I put all of the information about what this application is all about.</p>
+            </div>
         </div>
     )
 }
