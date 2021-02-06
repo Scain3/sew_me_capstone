4584576ea45537a602f902faf4a162bba32bfc97
Safiya Cain <say.cain3@gmail.com>
2021-02-06 12:44:04 -0500

    add first name and last name sign up form page

---
+++ frontend/src/components/SignupFormModal/index.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import SignupForm from './SignupForm';
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -7,6 +7,8 @@ import './SignupForm.css';
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -34,6 +36,24 @@ function SignupFormPage() {
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
+        <label>
+            First Name
+            <input
+                type="text"
+                value={firstname}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+            />
+        </label>
+        <label>
+            Last Name
+            <input
+                type="text"
+                value={lastname}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+            />
+        </label>
         <label>
           Email
           <input
