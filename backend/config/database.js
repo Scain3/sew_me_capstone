ad40522b9434bfbe433f759bf33c6de0da547559
Safiya Cain <say.cain3@gmail.com>
2021-02-08 12:28:14 -0500

    update production in config db file

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,8 +16,15 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    seederStorage: "sequelize",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
+
 };
