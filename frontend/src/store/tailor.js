184b9be2c3a676fb4ad54d67559c88156f154034
Safiya Cain <say.cain3@gmail.com>
2021-02-09 16:39:15 -0500

    create delete thunk function

--- frontend/src/store/tailor.js
+++ frontend/src/store/tailor.js
@@ -2,10 +2,19 @@ import {fetch} from './csrf.js';
 
 const TAILOR_PRODUCT = 'TAILOR_PRODUCTS';
 
+const REMOVE_PRODUCT = '/REMOVE_PRODUCT';
+
 const tailorProducts = (tailors) => ({
     type: TAILOR_PRODUCT,
     payload: tailors
 })
+
+const removeProduct = (id) => {
+    return{
+        type: REMOVE_PRODUCT,
+        payload: id
+    }
+}
 
 export const tailorProductInfo = (id, userId) => async(dispatch) => {
     const res = await fetch(`/api/tailor/${id}`, {
@@ -18,6 +27,13 @@ export const tailorProductInfo = (id, userId) => async(dispatch) => {
     })
     dispatch(tailorProducts(res.data.tailorItem))
 }
+
+export const removeTailoredProduct = (id) => async(dispatch) => {
+    await fetch(`/api/tailor/${id}`, {
+        method: 'DELETE'
+    });
+    dispatch(removeProduct(id))
+}
 const initialState = {
 
 }
@@ -25,6 +41,10 @@ const tailorReducer = (state=initialState, action) => {
     switch(action.type){
         case TAILOR_PRODUCT:
             return {...state, [action.payload.id]: action.payload};
+        case REMOVE_PRODUCT:
+            const newState = {...state}
+            delete newState[action.payload]
+            return newState;
         default:
             return state;
     }
