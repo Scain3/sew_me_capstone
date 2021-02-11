9f6014e76bb89240768dfd6c61c9ac42d921f5ed
Safiya Cain <say.cain3@gmail.com>
2021-02-11 17:48:06 -0500

    attempt to add addToCart functionality to tailor component

--- frontend/src/components/TailorPage/tailor.js
+++ frontend/src/components/TailorPage/tailor.js
@@ -1,12 +1,27 @@
 import { useSelector, useDispatch} from "react-redux";
 import {removeTailoredProduct} from "../../store/tailor";
 
+import {useHistory, useParams} from "react-router-dom";
+import {addItemToCart} from "../../store/cart";
+
 import './Tailor.css';
 
 function Tailor(){
     const tailorItem = useSelector(state => state.tailor);
+    const user = useSelector(state => state.session.user);
+    const {id} = useParams();
+    const history = useHistory();
     const dispatch = useDispatch();
     console.log(tailorItem);
+
+    const moveToCart = () => {
+        if(!user){
+            return history.push('/login');
+        }
+        const tailorId = tailorItem[id].id;
+        dispatch(addItemToCart(tailorId));
+        history.push('/cart');
+    }
 
 
     return(
@@ -21,6 +36,7 @@ function Tailor(){
         <div className="tailor-fabric">
             <div>{tailorItem.fabricColor}</div>
             <img src={tailorItem.fabricImage} alt={tailorItem.fabricType} />
+            <div><button onClick={moveToCart}>Add To Cart</button></div>
         </div>
 
     </div>
