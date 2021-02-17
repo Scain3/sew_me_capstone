59cd128edabdda9478566015f3f001eb0f776e0e
Safiya Cain <say.cain3@gmail.com>
2021-02-17 16:08:01 -0500

    create demo user sign in link

---
+++ frontend/src/components/DemoLogin/DemoLogin.js
@@ -0,0 +1,17 @@
+// import {useState} from "react";
+// import {login} from '../../store/session';
+
+// function demoLogin(){
+//     const [errors, setErrors] = useState([]);
+
+//     const credential = 'demo';
+//     const password = 'password';
+//     return dispatchEvent(login({credential, password}))
+//         .catch((res) => {
+//             if (res.data && res.data.errors) setErrors(res.data.errors);
+//         });
+
+
+// }
+
+// export default demoLogin;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
@@ -8,9 +8,22 @@ import './Navigation.css';
 import SearchBar from '../Search/SearchBar';
 import ShoppingCartIcon from '../ShoppingCart/ShoppingIcon';
 import SecondNav from '../SecondNav/SecondNav';
+import {login} from '../../store/session';
+
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+  const [errors, setErrors] = useState([]);
+
+  const demoLogin = () => {
+    const credential = 'Demo-lition';
+    const password = 'password';
+    return dispatch(login({credential, password}))
+        .catch((res) => {
+            if (res.data && res.data.errors) setErrors(res.data.errors);
+        });
+  }
 
   let sessionLinks;
   if (sessionUser) {
@@ -27,11 +40,15 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <span className="auth-links">
+        <button className="Demo-login" onClick={demoLogin}>Demo Login</button>
         <LoginFormModal />
         <NavLink className="signup" to="/signup">Sign Up</NavLink>
       </span>
     );
   }
+
+
+
 
   return (
     <div>
