7807bee021046d437bcd714afbb17a26b5a7a698
Safiya Cain <say.cain3@gmail.com>
2021-02-22 11:05:23 -0500

    create product page component

--- frontend/src/components/ProductPage/Patterns.js
+++ frontend/src/components/ProductPage/Patterns.js
@@ -1,12 +1,36 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {useParams,useHistory} from "react-router-dom";
+import {tailorProductInfo} from "../../store/tailor";
 import "./Product.css";
+
 function Patterns(){
     const patterns = useSelector(state => Object.values(state.patterns));
-    console.log(patterns);
+    const user = useSelector(state => state.session.user);
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleClick = () => {
+        if(!user){
+            return history.push('/login');
+        }
+        const patternId = patterns[id].id;
+        dispatch(tailorProductInfo(patternId, user.id))
+        history.push('/tailor')
+    }
 
     return(
-        <div>
-            hello World
+        <div className="product-container" >
+            {patterns.map((pattern, index) => (
+                <div key={index} >
+                    <div className="pattern-block" onClick={()=>history.push(`/patterns/${pattern.id}`)}>
+                        <div>{`${pattern.patternCompany} ${pattern.patternNumber}`}</div>
+                        <img className="images" src={pattern.image1} alt={ `${pattern.patternCompany} ${pattern.patternNumber}`} />
+                        <div>${pattern.price}</div>
+                    </div>
+                    <div><button onClick={handleClick}>tailor-this</button></div>
+                </div>
+            ))}
         </div>
     )
 
