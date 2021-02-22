b4f4842e8e5920e62df92223505740c30285a4e4
Safiya Cain <say.cain3@gmail.com>
2021-02-22 11:16:24 -0500

    style product page

--- frontend/src/components/ProductPage/Patterns.js
+++ frontend/src/components/ProductPage/Patterns.js
@@ -14,9 +14,9 @@ function Patterns(){
             {patterns.map((pattern, index) => (
                 <div key={index} >
                     <div className="pattern-block" onClick={()=>history.push(`/patterns/${pattern.id}`)}>
-                        <div>{`${pattern.patternCompany} ${pattern.patternNumber}`}</div>
-                        <img className="images" src={pattern.image1} alt={ `${pattern.patternCompany} ${pattern.patternNumber}`} />
-                        <div>${pattern.price}</div>
+                        <div className="header">{`${pattern.patternCompany} ${pattern.patternNumber}`}</div>
+                        <div><img className="images" src={pattern.image1} alt={ `${pattern.patternCompany} ${pattern.patternNumber}`} /></div>
+                        <div className="price">${pattern.price}</div>
                     </div>
                 </div>
             ))}
