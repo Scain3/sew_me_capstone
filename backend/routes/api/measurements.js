af2b0e1d9ec375e8275b56c24de7d8ed2d5da49d
Safiya Cain <say.cain3@gmail.com>
2021-02-23 16:02:23 -0500

    create measurements create backend router

--- backend/routes/api/measurements.js
+++ backend/routes/api/measurements.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { default: Measurements } = require('../../../frontend/src/components/Measurements/Measurements');
 const router = express.Router();
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 const { Measurement } = require("../../db/models");
@@ -10,5 +11,31 @@ router.get('/', asyncHandler(async(req, res) => {
         measurements
     )
 }));
+
+//Add your measurements
+router.post('/add-measurements', asyncHandler(async(req, res, next) => {
+
+    const measurements = await Measurements.create({
+        chest: req.body.chest,
+        shoulders: req.body.shoulders,
+        sleeves: req.body.sleeves,
+        biceps: req.body.biceps,
+        wrist: req.body.wrist,
+        waist: req.body.waist,
+        jacketLength: req.body.jacketLength,
+        height: req.body.height,
+        hip: req.body.hip,
+        thighCircumference: req.body.thighCircumference,
+        kneeCircumference: req.body.kneeCircumference,
+        waistToAnkle: req.body.waistToAnkle,
+        ankleToKnee: req.body.ankleToKnee,
+        kneeToThigh: req.body.kneeToThigh,
+        frontRise: req.body.frontRise,
+        backRise: req.body.backRise,
+    })
+    res.json({
+        measurements
+    })
+}))
 
 module.exports = router;
