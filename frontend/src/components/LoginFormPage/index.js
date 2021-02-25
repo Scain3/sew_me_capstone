3c42903a32a99b14f0713f9a276ff35d3c96d73c
Safiya Cain <say.cain3@gmail.com>
2021-02-25 17:13:48 -0500

    style login page vs login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -24,41 +24,43 @@ function LoginFormPage() {
 
   return (
     <div className="all-login-div">
-      <h1 className="login-form_heading">Log In</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <div className="login-form_input-container">
-          <label className="login-form_input-label">
-            Email
-          </label>
-          <input
-            className="login-form_input-box"
-            name="email"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="login-form_input-container">
-          <label className="login-form_input-label">
-            Password
-          </label>
+      <h1 className="login-formpage_heading">Log In</h1>
+      <div className="login-div">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <div className="login-formpage_input-container">
+            <label className="login-form_input-label">
+              Email
+            </label>
             <input
               className="login-form_input-box"
-              name="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
-        </div>
-        <button className="login-form_submit" type="submit">Log In</button>
-      </form>
+          </div>
+          <div className="login-form_input-container">
+            <label className="login-form_input-label">
+              Password
+            </label>
+              <input
+                className="login-form_input-box"
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+          </div>
+          <button className="form-submit_login" type="submit">Log In</button>
+        </form>
+      </div>
     </div>
   );
 }
