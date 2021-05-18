cc420119ab9594b9bc670f70428b9827877ef483
Safiya Cain <safiyacain@safiyas-air.attlocal.net>
2021-05-18 12:28:37 -0400

    add media screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
+          <Route path="/" >
             <LoginFormPage />
           </Route>
           <Route path="/signup">
